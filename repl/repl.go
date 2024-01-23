e8290516e02e46bbc01881b54b1163f1cf1b14de
Diego <diegolara93345@gmail.com>
2024-01-22 22:48:36 -0600

    evaluated booleans and integers

--- repl/repl.go
+++ repl/repl.go
@@ -2,6 +2,7 @@ package repl
 
 import (
 	"bufio"
+	"ch1/evaluator"
 	"ch1/lexer"
 	"ch1/parser"
 	"fmt"
@@ -41,8 +42,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
